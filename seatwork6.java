2912b11799a057595e3354ac2c7cde71c807dd2c
jenelynsopaled-ai <jenelynsopaled@gmail.com>
2025-08-30 08:32:05 +0000

    m

--- seatwork6.java
+++ seatwork6.java
@@ -1,1 +1,13 @@
-
+public class Seatwork6{
+    public static void main (String [] args) {
+        int dice = 1;
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzty.");
+            } else {
+                System.out.println("No Yatzty!");
+            }
+            dice = dice + 1;
+        }
+    }
+}
