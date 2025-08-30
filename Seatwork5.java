368a228d6531dfa2506e4ea97b54be012de90208
jenelynsopaled-ai <jenelynsopaled@gmail.com>
2025-08-30 08:22:55 +0000

    n

--- Seatwork5.java
+++ Seatwork5.java
@@ -1,1 +1,14 @@
-
+public class main {
+    public static void main (String[] args) {
+        System.out.println("SEATWORK");
+       
+       int countdown = 3;
+      
+      while (countdown > 0) {
+        System.out.println(countdown);
+        countdown--;
+      }
+     
+     System.out.println("Happy New Year!!");
+    }
+}
